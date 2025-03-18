f952168591a9309b1b4d8ebaeb0e9be61bea64c6
blaisegoze <blaisegoze@gmail.com>
2025-03-18 00:48:43 -0500

    Fixed height with dvh

--- js/index.js
+++ js/index.js
@@ -58,19 +58,6 @@ const navScrollHighlight = () => {
 }
 
 
-////// Hero Section Hight //////
-
-const heroSectionHight = () => {
-    // We listen to the resize event
-    window.addEventListener('resize', () => {
-        // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-        let vh = window.innerHeight * 0.01;
-        // Then we set the value in the --vh custom property to the root of the document
-        document.documentElement.style.setProperty('--vh', `${vh}px`);
-    });
-}
-
-
 ////// Nav Portfolio //////
 
 const navPortfolio = () => {
@@ -124,6 +111,5 @@ onclick = ()=>{
 
 navScroll();
 navScrollHighlight();
-heroSectionHight();
 navPortfolio();
 //websiteAlert();
