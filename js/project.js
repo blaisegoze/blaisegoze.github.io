78e6ca80943fa78683b78aa8d4d529c21ef3cc33
blaisegoze <blaisegoze@gmail.com>
2025-04-03 15:32:50 -0500

    Added projects, content, & fixed YouTube display size

--- js/project.js
+++ js/project.js
@@ -13,7 +13,7 @@ const portfolioPageOrder = () => {
     //  }, false);
     // }
 
-    const pagesAll = [{page: '/pure', type: '3d video'}, {page: '/variance', type: '2d'}, {page: '/overcome', type: '3d video'}, {page: '/davids', type: '3d'}, {page: '/nepenthe', type: '2d'}, {page: '/fantasy_tavern', type: '3d'}, {page: '/mutant_opie', type: '2d'}, {page: '/watermelon', type: '3d video'}, {page: '/logos', type: '2d'}, {page: '/lollipop', type: '3d'}, {page: '/balloon', type: '3d video'}, {page: '/la_rosa', type: '3d'}, {page: '/build_a_pet', type: '2d'}, {page: '/free_yourself', type: '2d'}, {page: '/turndown_cookie', type: 'video'}, {page: '/weddings', type: 'video'}];
+    const pagesAll = [{page: '/px200r', type: 'video'}, {page: '/infinity_air', type: '3d video'}, {page: '/pure', type: '3d video'}, {page: '/variance', type: '2d'}, {page: '/overcome', type: '3d video'}, {page: '/davids', type: '3d'}, {page: '/nepenthe', type: '2d'}, {page: '/fantasy_tavern', type: '3d'}, {page: '/mutant_opie', type: '2d'}, {page: '/watermelon', type: '3d video'}, {page: '/logos', type: '2d'}, {page: '/lollipop', type: '3d'}, {page: '/balloon', type: '3d video'}, {page: '/la_rosa', type: '3d'}, {page: '/build_a_pet', type: '2d'}, {page: '/free_yourself', type: '2d'}, {page: '/turndown_cookie', type: 'video'}, {page: '/weddings', type: 'video'}];
 
     if (sessionStorage.getItem('mediaType') == 'all' || sessionStorage.getItem('mediaType') == null){
         var filteredPagesArray = pagesAll;
